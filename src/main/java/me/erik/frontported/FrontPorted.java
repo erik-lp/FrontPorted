87b1e27fd0320590776231dc2f20e6f6a61d2af4
erik-lp <erik-berlin@outlook.de>
2021-04-05 22:13:09 +0200

    minor changes

--- src/main/java/me/erik/frontported/FrontPorted.java
+++ src/main/java/me/erik/frontported/FrontPorted.java
@@ -2,14 +2,28 @@ package me.erik.frontported;
 
 import me.erik.frontported.config.Config;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
+
+import java.io.File;
 
 public class FrontPorted implements ModInitializer {
     
+    /**The global config object containing all customizable values.*/
     public static final Config CONFIG = new Config();
+    
+    /**The directory in the config folder for all important files (like config.json)*/
+    public static File modDir;
     
     @Override
     public void onInitialize() {
+        
+        if (!modDir.exists()) {
+            //noinspection RedundantSuppression, ResultOfMethodCallIgnored
+            modDir.mkdirs();
+        }
+        
         CONFIG.init();
+        
     }
     
 }
